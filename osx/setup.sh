ef266e78f5e28f6e259deb5f808bdfdf28365059
Daniel Marcous <dmarcous@gmail.com>
2021-08-24 01:15:57 +0300

    update to 2021

--- osx/setup.sh
+++ osx/setup.sh
@@ -7,7 +7,13 @@
 sudo chmod -R a+w /usr/local/*
 brew update && brew upgrade
 ## Installs 
-brew install wget gcc llvm fish thefuck fzf grc gradle sbt maven git-lfs node tmux openvpn automake libtool protobuf tig swig gflags glog openssl openssh cmake bazel shellcheck jq coreutils findutils bash bash-completion2 grep
+brew install wget gcc llvm fish thefuck fzf grc gradle sbt maven git-lfs node tmux openvpn automake libtool protobuf tig swig gflags glog openssl openssh cmake bazel shellcheck jq coreutils findutils bash bash-completion2 grep bash bats-core terraform
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+brew tap sambadevi/powerlevel9k
+brew install powerlevel9k
+
+
 brew install caskroom/cask/brew-cask
 brew cask install google-cloud-sdk
 brew install kubectl
@@ -16,47 +22,71 @@ brew tap caskroom/cask
 brew cask install font-inconsolata
 brew cask install font-hack-nerd-font ### to use : change on iterm2 profile settings to use font = Hack nerd font)
 brew cask install insomnia
-$(brew --prefix)/opt/fzf/install
+
 ## Cleanup
 brew cleanup
 
 # Zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+brew install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Fish
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-fisher z fzf edc/bass grc
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/fish-bax jethrokuan/z orefalo/grc jethrokuan/fzf
 curl -L https://get.oh-my.fish | fish
+
+# fzf
+$(brew --prefix)/opt/fzf/install
+fzf
 
 # Python
 ## Setup root python
 sudo pip3 install --upgrade pip setuptools wheel six
+# Poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 ## -- Install Conda -- https://www.anaconda.com/distribution/
 conda init
 conda init fish
 ## After anaconda setup
-conda install imutils 
-conda install -c conda-forge awscli 
-conda install -c conda-forge google-cloud
-conda install -c conda-forge pyarrow
-conda install -c conda-forge clang_osx-64 clangxx_osx-64
+conda install -c conda-forge scikit-learn
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+conda install -c conda-forge tensorflow
+conda install -c conda-forge tensorboard
 ## Jupyter lab
+conda install -c conda-forge nodejs
 conda install -c conda-forge jupyterlab
+jupyter labextension install jupyterlab_tensorboard
+conda install -c conda-forge nbresuse
+pip install jupyterlab-system-monitor
+conda install -c conda-forge xeus-python
 conda install -c conda-forge jupyterlab-git
-conda install -c conda-forge ipywidgets
-conda install -c conda-forge black
-conda install -c pyviz/label/dev pyviz
-jupyter labextension install @jupyterlab/toc
-jupyter labextension install @jupyterlab/git
-jupyter serverextension enable --py jupyterlab_git
+conda install -c conda-forge jupytext
+jupyter labextension install nbgather
+jupyter labextension install @lckr/jupyterlab_variableinspector
+pip install nbdime
+jupyter labextension install jupyterlab_voyager
+pip install aquirdturtle_collapsible_headings
+
+# gcloud
+cd /tmp
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-353.0.0-darwin-x86_64.tar.gz 
+tar –xvzf google-cloud-sdk-353.0.0-darwin-x86_64.tar.gz
+cd google-cloud-sdk-353.0.0-darwin-x86_64
+./google-cloud-sdk/install.sh
+./google-cloud-sdk/bin/gcloud init
+gcloud auth application-default login
 
 # Mac
 defaults write com.apple.finder AppleShowAllFiles YES
 
 # Sublime setup
 sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
-defaults write com.apple.LaunchServices/com.apple.launchservices.secure LSHandlers -array-add \
-'{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.3;}'
+defaults write com.apple.LaunchServices LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.3;}'
 
+# Git config
+git config --global user.name "dmarcous"
+git config --global user.email "dmarcous@gmail.com"
+git config --global core.editor "subl -n -w"
